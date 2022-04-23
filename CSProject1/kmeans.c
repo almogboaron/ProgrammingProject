f7d859d0bc2e5f7a2fe14905cb5a6d8af4c9590a
almog <almogboaron@mail.tau.ac.il>
2022-04-23 21:40:50 +0300

    Commiting Finalized Prouduct of Kmeans Wasuup!

--- CSProject1/kmeans.c
+++ CSProject1/kmeans.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <limits.h>
+#include <float.h>
 
 /*Declorations*/
 int K;
@@ -12,8 +14,6 @@ int max_iter;
 int n;
 int d;
 double EPSILON = 0.001;
-int i,j;
-int idx;
 int iter_num = 0;
 int convergenceCount;
 int countcols=0;
@@ -34,6 +34,7 @@ double** dataAloc;
 
 /*Function Initalizeation Centroids -> Matrix Formation as first k data points */
 void Init_Centroids(){
+    int i;
 
     /*Initializing Centroids Mat*/
     for(i=0 ; i<K*d ;i++){
@@ -47,9 +48,9 @@ void Init_Centroids(){
 
 /*Function Read Files and initialize Matrix (n*d) of Information*/ 
 void init_DataMat(){
-    char *line = NULL, *number ;
     FILE* fp;
-    
+    int i,j;
+
     /*Initializing Pointer to Data Mat (k*d) (Or Vectors X1,...Xn == R1....Rn)*/
     for( i=0; i<n ; i++){
         dataAloc[i] = data + i*d ; 
@@ -58,20 +59,19 @@ void init_DataMat(){
     /*Initialize Data Matrix( Data Array n*d);*/
     fp = fopen(filename_in,"r");
     assert(fp!=NULL);
-    while(!feof(fp)){
-        fscanf(fp,"%s",line);
-        number = strtok(line,",");
-        while(number!=NULL){
-            data[i++] = (double)atof(number);
-            number = strtok(NULL,",");
+
+    for(i=0;i<n;i++){
+        for(j=0;j<d;j++){
+            fscanf(fp,"%lf",&dataAloc[i][j]);
+            fgetc(fp);
         }
     }
 }
 
 /*Initializeation of ClusterSum*/
 void init_Clusters(){
-    ClusterSum = calloc(K*d,sizeof(double));
-    ClusterSumAloc = calloc(K,sizeof(int));
+    int i;
+
     /*Initializing Centroids Mat*/
     for(i=0 ; i<K*d ;i++){
         ClusterSum[i] = 0;
@@ -85,35 +85,44 @@ void init_Clusters(){
 /* Function Cols Dims. */
 int numOfCols(){
     int countcols=0;
-    char *token,*line = NULL;
+    char ch;
     FILE *fp = fopen(filename_in,"r");
     assert(fp!=NULL);
-    fscanf(fp,"%s",line);
-    fclose(fp);
-    token = strtok(line,",");
-    while (token != NULL){
-        countcols++;
-        token = strtok(NULL,",");
+    
+    while(!feof(fp)){
+        ch = fgetc(fp);
+        if(ch == ','){
+            countcols++;
+        }
+        if(ch=='\n'){break;}
     }
+
+    countcols++;
+    fclose(fp);
     return countcols;
 
 }
 
 /*Counting Rows Dims.*/
 int numbOfRows(){
-    char *line = NULL;
+    char ch ;
     int countrows=0;
     FILE *fp = fopen(filename_in,"r");
     assert(fp!=NULL);
-    while(feof(fp)){
-        fscanf(fp,"%s",line);
-        ++countrows;
+    
+    while(!feof(fp)){
+        ch = fgetc(fp);
+        if(ch == '\n'){
+            countrows++;
+        }
     }
     fclose(fp);
     return countrows;
 }
+
 /*Sum Data Point with apropriate Cluster*/
 void SumWithCluster(double* clusterSum , double* dataPoint){
+    int i;
     for(i=0; i<d; i++){
         clusterSum[i] = clusterSum[i] + dataPoint[i];
     }
@@ -121,34 +130,35 @@ void SumWithCluster(double* clusterSum , double* dataPoint){
 
 /* Sum DataPoint with Apropritate Cluster Sum return idx of cluster*/
 int assign(double *datapoint){
-    double minDist = 1.7976931348623158E+308;
+    double minDist = DBL_MAX;
     double norm=0 ;
+    int i,j;
+    int idx;
 
     /*Go through all Centroids and choose the closest to dataPoint*/
-    for(j=0; j<K; j++){
+    for(i=0; i<K; i++){
+        norm = 0;
         
         /*Calculating Delta */
-        for(i=0; i<d; i++){
-            norm += datapoint[i] - CenetroidAloc[j][i];
+        for(j=0; j<d; j++){
+            norm += pow(datapoint[j] - CenetroidAloc[i][j],2);
+            
         }
 
-        /*Calculation Norm*/
-        norm = sqrt(norm);
-        
         /*Checking MinDistance*/
         if (norm < minDist){
             minDist = norm;
-            idx = j;
+            idx = i;
         }
     }
-
     /*Summing the DataPoint With the closest Cluster.*/
-   SumWithCluster(ClusterSumAloc[idx], datapoint);
-   return idx;
+    SumWithCluster(ClusterSumAloc[idx], datapoint);
+    return idx;
 }
 
 /*Devide Cluster with number of points*/
 void NormelizeClusterSums(int* counter){
+    int i,j;
     for(i=0; i<K; i++){
         for(j=0;j<d;j++){
             ClusterSumAloc[i][j] = ClusterSumAloc[i][j]/counter[i];
@@ -159,6 +169,7 @@ void NormelizeClusterSums(int* counter){
 /*Update Centroinds, Resets Cluster  And returns the EuclidianDistance.*/
 double update_centroid(double* Centroid , double* ClusterSum){
     double norm;
+    int i;
     for(i=0; i<d; i++){
         norm +=pow(Centroid[i] - ClusterSum[i],2);
         Centroid[i] = ClusterSum[i];
@@ -168,12 +179,15 @@ double update_centroid(double* Centroid , double* ClusterSum){
 }
 
 void WriteBackCentroids(){
+    int i,j;
     FILE *fp = fopen(filename_out,"w");
     assert(fp!=NULL);
+
     for(i=0; i<K ;i++){
         for(j=0; j<d; j++){
-                if(j==d-1){fprintf(fp,"%f,",CenetroidAloc[i][j]);}
-                else    {fprintf(fp,"%f",CenetroidAloc[i][j]);}
+                /*Not Last Cloumn*/
+                if(j<d-1){fprintf(fp,"%.4f,",CenetroidAloc[i][j]);}
+                else    {fprintf(fp,"%.4f",CenetroidAloc[i][j]);}
         }
         fprintf(fp,"%s","\n");
     }
@@ -182,32 +196,35 @@ void WriteBackCentroids(){
 
 /*Main Function of Kmeans Algorithem .*/
 int main(int argc, char *argv[]) {
-    
+    int i;
+    int idx;
+    /*Make Assertions!*/
+
     /*Without MaxIter*/
-    if (argc == 2) { 
-        K = atoi(argv[0]);
+    if (argc == 4) { 
+        K = atoi(argv[1]);
         max_iter = 200;
-        filename_in = argv[1];
-        filename_out = argv[2];
-    }
-    
-    /*With MaxIter*/
-    else { 
-        K = atoi(argv[0]);
-        max_iter = atoi(argv[1]);
         filename_in = argv[2];
-        filename_out = argv[3]; 
+        filename_out = argv[3];
+    }
+    /*With MaxIter*/
+    else {
+        K = atoi(argv[1]);
+        max_iter = atoi(argv[2]);
+        filename_in = argv[3];
+        filename_out = argv[4]; 
     }
 
     /*Initialization*/
     n = numbOfRows();
     d = numOfCols();
+
     ClusterSum = calloc(K*d,sizeof(double));
-    ClusterSumAloc = calloc(K,sizeof(int));
+    ClusterSumAloc = calloc(K,sizeof(double));
     Centroids = calloc(K*d,sizeof(double));
-    CenetroidAloc = calloc(K,sizeof(int));
+    CenetroidAloc = calloc(K,sizeof(double));
     data = calloc(n*d,sizeof(double));
-    dataAloc = calloc(n,sizeof(int));
+    dataAloc = calloc(n,sizeof(double));
 
     init_DataMat();
     Init_Centroids();
@@ -215,6 +232,7 @@ int main(int argc, char *argv[]) {
     
     count = calloc(K,sizeof(int));
     while (iter_num < max_iter){
+
         /*Reset:countPerCluster , ConvergenceCount;*/
         convergenceCount = 0;
         memset(count, 0, K*sizeof(int));
@@ -224,7 +242,6 @@ int main(int argc, char *argv[]) {
             idx = assign(dataAloc[i]);
             count[idx] += 1;
         }
-        
         /*Normalize Cluster*/
         NormelizeClusterSums(count);
 
@@ -235,10 +252,12 @@ int main(int argc, char *argv[]) {
                 convergenceCount+=1;
             }
         }
+
         /*Convergence or Continue Iteration;*/
         if (convergenceCount == K){break;}
         iter_num++;
     }
+
 WriteBackCentroids();
 free(ClusterSum);
 free(ClusterSumAloc);
